8b6fc78f67979668da045df6e8ca4a9342000218
SSAFY <lns960712@naver.com>
2022-03-02 17:40:38 +0900

    start project

---
+++ backend/docong/src/main/java/com/b5f1/docong/DocongApplication.java
@@ -0,0 +1,13 @@
+package com.b5f1.docong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocongApplication.class, args);
+	}
+
+}
---
+++ backend/docong/src/test/java/com/b5f1/docong/DocongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b5f1.docong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
