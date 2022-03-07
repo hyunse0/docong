034b37bef3fc5148d5b6f7b6de9182e564a2be3e
김영훈 <rladudgns456@naver.com>
2022-03-07 12:44:24 +0900

    [S06P21S003-99] feat: Pomodoro Controller 작성 - PomodoroController.java - SavePomodoroReqDto.java

---
+++ backend/docong/src/main/java/com/b5f1/docong/api/dto/request/SavePomodoroReqDto.java
@@ -0,0 +1,32 @@
+package com.b5f1.docong.api.dto.request;
+
+import com.b5f1.docong.core.domain.pomodoro.Emotion;
+import com.b5f1.docong.core.domain.pomodoro.TimeStatus;
+import com.b5f1.docong.core.domain.pomodoro.noiseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SavePomodoroReqDto {
+
+    @NotNull
+    private Long user_seq;
+    @NotNull
+    private Long todo_seq;
+    @NotNull
+    private TimeStatus timeStatus;
+    @NotNull
+    private LocalDateTime startTime;
+    @NotNull
+    private LocalDateTime endTime;
+    @NotNull
+    private Emotion emotion;
+    @NotNull
+    private noiseStatus noise;
+}
