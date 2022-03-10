2ecce4adf9e625f8947a90ae163054d56851cdd8
김영훈 <rladudgns456@naver.com>
2022-03-10 14:44:02 +0900

    [S06P21S003-93] feat: Pomodoro와 회원 할일 연동

--- backend/docong/src/main/java/com/b5f1/docong/core/domain/pomodoro/Pomodoro.java
+++ backend/docong/src/main/java/com/b5f1/docong/core/domain/pomodoro/Pomodoro.java
@@ -2,6 +2,7 @@ package com.b5f1.docong.core.domain.pomodoro;
 
 import com.b5f1.docong.core.domain.BaseEntity;
 import com.b5f1.docong.core.domain.todo.Todo;
+import com.b5f1.docong.core.domain.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,18 +11,22 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Pomodoro extends BaseEntity {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long seq;
 
+
     @Builder
-    public Pomodoro(Long seq, Long user_seq, Todo todo, TimeStatus timeStatus, LocalDateTime startTime, LocalDateTime endTime, Emotion emotion, noiseStatus noise) {
+    public Pomodoro(Long seq, User user, Todo todo, TimeStatus timeStatus, LocalDateTime startTime, LocalDateTime endTime, Emotion emotion, noiseStatus noise) {
         this.seq = seq;
-        this.user_seq = user_seq;
+        this.user = user;
         this.todo = todo;
         this.timeStatus = timeStatus;
         this.startTime = startTime;
@@ -30,14 +35,14 @@ public class Pomodoro extends BaseEntity {
         this.noise = noise;
     }
 
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "USER_SEQ")
+    private User user;
 
-    //    @ManyToOne
-//    @JoinColumn(name = "USER_SEQ")
-    private Long user_seq;
-
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "TODO_SEQ")
     private Todo todo;
+
     @Enumerated(EnumType.STRING)
     private TimeStatus timeStatus;
     private LocalDateTime startTime;
@@ -47,7 +52,6 @@ public class Pomodoro extends BaseEntity {
     private Emotion emotion;
     @Enumerated(EnumType.STRING)
     private noiseStatus noise;
-
 
 
 }
---
+++ backend/docong/src/test/java/com/b5f1/docong/api/service/PomodoroServiceImplTest.java
@@ -0,0 +1,108 @@
+package com.b5f1.docong.api.service;
+
+import ch.qos.logback.core.spi.LogbackLock;
+import com.b5f1.docong.api.dto.request.SavePomodoroReqDto;
+import com.b5f1.docong.api.dto.request.SaveTodoReqDto;
+import com.b5f1.docong.api.dto.request.UserInfoReqDto;
+import com.b5f1.docong.core.domain.group.Team;
+import com.b5f1.docong.core.domain.pomodoro.Emotion;
+import com.b5f1.docong.core.domain.pomodoro.Pomodoro;
+import com.b5f1.docong.core.domain.pomodoro.TimeStatus;
+import com.b5f1.docong.core.domain.pomodoro.noiseStatus;
+import com.b5f1.docong.core.domain.todo.Todo;
+import com.b5f1.docong.core.domain.todo.UserTodo;
+import com.b5f1.docong.core.domain.user.User;
+import com.b5f1.docong.core.repository.PomodoroRepository;
+import com.b5f1.docong.core.repository.TodoRepository;
+import com.b5f1.docong.core.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PomodoroServiceImplTest {
+    @Autowired
+    private PomodoroService pomodoroService;
+
+    @Autowired
+    private PomodoroRepository pomodoroRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    private User savedUser;
+    private Todo savedTodo;
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    void before() {
+        createUser();
+        createTodo();
+    }
+
+    @Test
+    void 뽀모도로저장하기() {
+        //given
+        SavePomodoroReqDto savePomodoroReqDto = new SavePomodoroReqDto(savedUser.getSeq(), savedTodo.getSeq(), TimeStatus.BASIC, null, null, Emotion.ANGER, noiseStatus.NOISE);
+
+        //when
+        Long saveSeq = pomodoroService.savePomodoro(savePomodoroReqDto);
+        Optional<Pomodoro> save = pomodoroRepository.findById(saveSeq);
+
+        //then
+        assertThat(save.get().getSeq()).isEqualTo(saveSeq);
+
+    }
+
+    @Test
+    void 유저아이디로뽀모도로리스트찾기() {
+        //given
+        Long seq = pomodoroService.savePomodoro(new SavePomodoroReqDto(savedUser.getSeq(), savedTodo.getSeq(), TimeStatus.BASIC, null, null, Emotion.ANGER, noiseStatus.NOISE));
+
+        //when
+        List<Pomodoro> findByUser = pomodoroService.findAll(savedUser.getSeq());
+
+        //then
+        assertThat(findByUser.get(0).getSeq()).isEqualTo(seq);
+        
+    }
+
+    private void createUser() {
+        User user = User.builder()
+                .email("wjddma1214@gmail.com")
+                .password("12345")
+                .build();
+        savedUser = userRepository.save(user);
+    }
+
+    private void createTodo() {
+        User user = userRepository.findByEmail("wjddma1214@gmail.com");
+
+        SaveTodoReqDto reqDto = new SaveTodoReqDto("제목", "내용", null, user.getSeq(), null, null, null);
+        Todo todo = reqDto.toEntity();
+        UserTodo userTodo = UserTodo.builder()
+                .build();
+
+        todo.addUserTodo(userTodo);
+        user.addUserTodo(userTodo);
+
+        savedTodo = todoRepository.save(todo);
+    }
+
+
+}
