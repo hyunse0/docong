524bba0dfc8d27ddf6a1762436117a5e1e759f30
유혜승 <hyeseung0124@gmail.com>
2022-03-15 14:36:11 +0900

    chore: 중복되는 기능 제거(이메일 중복검사)
    join시 email 중복검사 기능 제거

--- backend/docong/src/main/java/com/b5f1/docong/api/service/UserServiceImpl.java
+++ backend/docong/src/main/java/com/b5f1/docong/api/service/UserServiceImpl.java
@@ -22,19 +22,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void join(JoinReqDto joinReqDto) {
-        // email 중복체크
-        User user = userRepository.findByEmailAndActivateTrue(joinReqDto.getEmail());
-
         // gmail 가입 시도 시 oAuth를 이용해달라고 하기
         String emailPart = joinReqDto.getEmail().trim().split("@")[1];
         if (emailPart.equals("gmail.com")) {
             // 에러 던지기
             System.out.println("gmail은 Google 로그인을 이용해주세요.");
-        } else if (user != null) {
-            // 에러 던지기
-            System.out.println("이미 존재하는 email입니다.");
-        } else if (user == null) {
-            user = User.builder()
+        } else {
+            User user = User.builder()
                     .email(joinReqDto.getEmail())
                     .password(bCryptPasswordEncoder.encode(joinReqDto.getPassword()))
                     .name(joinReqDto.getName())
