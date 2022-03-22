4a109bd8807efc4564b16eb3ae8a6ba138e04c28
유혜승 <hyeseung0124@gmail.com>
2022-03-22 13:24:39 +0900

    [S06P21S003-170] feat: User Request Dto Validation 추가
    User관련 Request Dto Validation 추가

--- backend/docong/src/main/java/com/b5f1/docong/api/dto/request/EmailCheckReqDto.java
+++ backend/docong/src/main/java/com/b5f1/docong/api/dto/request/EmailCheckReqDto.java
@@ -2,7 +2,10 @@ package com.b5f1.docong.api.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class EmailCheckReqDto {
+    @NotNull
     String email;
 }
--- backend/docong/src/main/java/com/b5f1/docong/api/dto/request/JoinReqDto.java
+++ backend/docong/src/main/java/com/b5f1/docong/api/dto/request/JoinReqDto.java
@@ -2,9 +2,13 @@ package com.b5f1.docong.api.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class JoinReqDto {
+    @NotNull
     String email;
+    @NotNull
     String password;
     String name;
     String birth;
