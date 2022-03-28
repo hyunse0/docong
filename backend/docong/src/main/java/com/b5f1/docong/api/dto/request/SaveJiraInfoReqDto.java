c84d7d9b751907292e23626b056678babfabf0bc
유혜승 <hyeseung0124@gmail.com>
2022-03-28 14:41:40 +0900

    fix: DTO 빈 생성자 생성
    DTO에 빈 생성자가 없어 발생하는 오류 해결

--- backend/docong/src/main/java/com/b5f1/docong/api/dto/request/SaveJiraInfoReqDto.java
+++ backend/docong/src/main/java/com/b5f1/docong/api/dto/request/SaveJiraInfoReqDto.java
@@ -2,9 +2,11 @@ package com.b5f1.docong.api.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SaveJiraInfoReqDto {
     String jiraDomain;
     String jiraUserId;
