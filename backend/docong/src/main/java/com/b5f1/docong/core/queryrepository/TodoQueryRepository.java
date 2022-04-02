055c8099cadd67a8b2a91bcfb4644e7973cce115
jeongum <aldlswjddma@naver.com>
2022-04-02 23:45:51 +0900

    fix: Team 삭제 시, Todo Null처리 및 delete처리

--- backend/docong/src/main/java/com/b5f1/docong/core/queryrepository/TodoQueryRepository.java
+++ backend/docong/src/main/java/com/b5f1/docong/core/queryrepository/TodoQueryRepository.java
@@ -1,10 +1,12 @@
 package com.b5f1.docong.core.queryrepository;
 
 import com.b5f1.docong.api.dto.response.FindTodoResDto;
+import com.b5f1.docong.core.domain.todo.Todo;
 
 import java.util.List;
 
 public interface TodoQueryRepository {
     List<FindTodoResDto> findAllWithUserId(Long userSeq);
     List<FindTodoResDto> findAllWithGroupId(Long groupSeq);
+    List<Todo> findTodosWithTeamSeq(Long teamSeq);
 }
