4a4bf58ae73e52dd331ef75725e5782f7c32df04
SSAFY <gkgustj@naver.com>
2022-04-03 18:01:25 +0900

    [S06P21S003-259] fix: TodoQueryRepo 수정

--- backend/docong/src/main/java/com/b5f1/docong/core/queryrepository/TodoQueryRepository.java
+++ backend/docong/src/main/java/com/b5f1/docong/core/queryrepository/TodoQueryRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface TodoQueryRepository {
     List<FindTodoResDto> findAllWithUserId(Long userSeq);
     List<FindTodoResDto> findAllWithGroupId(Long groupSeq);
-    List<FindTodoResDto> findAllWithUserIdAndGroupId(Long userSeq, Long groupSeq);
     List<Todo> findTodosWithTeamSeq(Long teamSeq);
+    List<FindTodoResDto> findAllWithUserIdAndGroupId(Long userSeq, Long groupSeq);
 }
