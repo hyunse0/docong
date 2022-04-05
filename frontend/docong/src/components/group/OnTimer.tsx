eca22aeafd9f8bd1e09b33377b03c632c9ddac67
Hui-Story <leejaehee0728@gmail.com>
2022-04-06 02:20:20 +0900

    [S06P21S003-278][S06P21S003-281] 시각화 마무리 및 UI/UX 개선
    업무 카테고리 통계 전체 목록이 보이게 수정
    NavBar 그룹 생성, 목록 구현
    그룹 Todo 페이지 레이아웃 수정

--- frontend/docong/src/components/group/OnTimer.tsx
+++ frontend/docong/src/components/group/OnTimer.tsx
@@ -1,54 +1,48 @@
-import { Avatar, Box, Tooltip } from "@mui/material"
-import { OnOffUser, OnOffUserList } from "../../api/group"
-import OnTimerDetail from "./OnTimerDetail"
+import { Avatar, Box, Tooltip } from '@mui/material'
+import { OnOffUser, OnOffUserList } from '../../api/group'
+import OnTimerDetail from './OnTimerDetail'
 
 interface OnTimerProps {
-    userList: OnOffUserList | null
-    getUserList: () => void
+  userList: OnOffUserList | null
+  getUserList: () => void
 }
 
 function OnTimer({ userList, getUserList }: OnTimerProps) {
-
-    return (
-        <Box
-            sx={{
-                flexDirection: 'row',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'end',
-                height: '9%',
-            }}>
-            {
-                userList !== null &&
-                userList
-                    .filter((user: OnOffUser) => user.online === true)
-                    .map((user: OnOffUser) => (
-                        <Tooltip title={`${user.userName}`}>
-                            <Avatar
-                                sx={{
-                                    width: 24,
-                                    height: 24,
-                                    m: 0.5,
-                                    flexDirection: 'column',
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'end',
-                                }}
-                                alt={`${user.userName}`}
-                                src={
-                                    user.userImg
-                                        ? user.userImg
-                                        : '/images/Profile_Default.png'
-                                }
-                            />
-                        </Tooltip>
-                    ))
-            }
-            <OnTimerDetail
-                userList={userList}
-                getUserList={getUserList} />
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        flexDirection: 'row',
+        display: 'flex',
+        justifyContent: 'end',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      {userList !== null &&
+        userList
+          .filter((user: OnOffUser) => user.online === true)
+          .map((user: OnOffUser) => (
+            <Tooltip title={`${user.userName} (${user.userEmail})`}>
+              <Avatar
+                sx={{
+                  width: 24,
+                  height: 24,
+                  m: 0.5,
+                  flexDirection: 'column',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'end',
+                }}
+                alt={`${user.userName}`}
+                src={
+                  user.userImg ? user.userImg : '/images/Profile_Default.png'
+                }
+              />
+            </Tooltip>
+          ))}
+      <OnTimerDetail userList={userList} getUserList={getUserList} />
+    </Box>
+  )
 }
 
 export default OnTimer
--- frontend/docong/src/containers/group/GroupTodoContainer.tsx
+++ frontend/docong/src/containers/group/GroupTodoContainer.tsx
@@ -1,202 +1,196 @@
-import { Avatar, Box, Button, Tooltip } from '@mui/material'
+import { Box, Button, Grid } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Group, MemberData, OnOffUser } from '../../api/group'
+import { Group } from '../../api/group'
 import { GroupTodo, GroupTodoInput } from '../../api/groupTodo'
 import GroupTodoComponent from '../../components/group/GroupTodoComponent'
 import { RootState } from '../../modules'
-import { deleteGroupTodoAsync, findAllGroupTodosAsync, modifyGroupTodoAsync, saveGroupTodoAsync } from '../../modules/groupTodo'
+import {
+  deleteGroupTodoAsync,
+  findAllGroupTodosAsync,
+  modifyGroupTodoAsync,
+  saveGroupTodoAsync,
+} from '../../modules/groupTodo'
 import { modifyTodoStatusAsync } from '../../modules/todo'
 import { changeUserTimerTodo } from '../../modules/user'
-import produce from 'immer'
 import { getUserListDataAsync } from '../../modules/group'
-import OnTimerDetail from '../../components/group/OnTimerDetail'
 import OnTimer from '../../components/group/OnTimer'
 
 function GroupTodoContainer() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const params = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const params = useParams()
 
-    const groupSeq = Number(params.groupSeq)
-    const groups = useSelector((state: RootState) => state.group.groups.data)
-    const groupTodos = useSelector((state: RootState) => state.groupTodo.groupTodos.data)
-    const userList = useSelector((state: RootState) => state.group.userList.data)
-    const [group, setGroup] = useState<null | Group>(null)
+  const [groupSeq, setGroupSeq] = useState(Number(params.groupSeq))
+  //   const groupSeq = Number(params.groupSeq)
+  const groups = useSelector((state: RootState) => state.group.groups.data)
+  const groupTodos = useSelector(
+    (state: RootState) => state.groupTodo.groupTodos.data
+  )
+  const userList = useSelector((state: RootState) => state.group.userList.data)
+  const [group, setGroup] = useState<null | Group>(null)
 
-    useEffect(() => {
-        if (groups != null) {
-            const matchGroup = groups
-                .find(g => g.teamSeq === groupSeq);
-            setGroup({
-                teamSeq: matchGroup ? matchGroup.teamSeq : 0,
-                jiraApiToken: matchGroup ? matchGroup.jiraApiToken : '',
-                jiraDomain: matchGroup ? matchGroup.jiraDomain : '',
-                jiraProjectKey: matchGroup ? matchGroup.jiraProjectKey : '',
-                jiraUserId: matchGroup ? matchGroup.jiraUserId : '',
-                userList: matchGroup ? matchGroup.userList : null,
-                name: matchGroup ? matchGroup.name : '',
-                leaderEmail: matchGroup ? matchGroup.leaderEmail : ''
-            })
-        }
-    }, [groups])
-
-    useEffect(() => {
-        findGroupTodos()
-        getUserList()
-    }, [])
-
-    const findGroupTodos = () => {
-        dispatch(findAllGroupTodosAsync.request(groupSeq))
+  useEffect(() => {
+    if (groups != null) {
+      const matchGroup = groups.find((g) => g.teamSeq === groupSeq)
+      setGroup({
+        teamSeq: matchGroup ? matchGroup.teamSeq : 0,
+        jiraApiToken: matchGroup ? matchGroup.jiraApiToken : '',
+        jiraDomain: matchGroup ? matchGroup.jiraDomain : '',
+        jiraProjectKey: matchGroup ? matchGroup.jiraProjectKey : '',
+        jiraUserId: matchGroup ? matchGroup.jiraUserId : '',
+        userList: matchGroup ? matchGroup.userList : null,
+        name: matchGroup ? matchGroup.name : '',
+        leaderEmail: matchGroup ? matchGroup.leaderEmail : '',
+      })
     }
+  }, [groups, groupSeq])
 
-    const getUserList = () => {
-        dispatch(getUserListDataAsync.request(groupSeq))
-    }
+  useEffect(() => {
+    setGroupSeq(Number(params.groupSeq))
+  }, [params.groupSeq])
 
-    const onClickToGroupAnalysis = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        navigate(`/group/analysis/${groupSeq}`)
-    }
+  useEffect(() => {
+    findGroupTodos()
+    getUserList()
+  }, [groupSeq])
 
-    const onClickToGroup = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        navigate(`/group`)
-    }
+  const findGroupTodos = () => {
+    dispatch(findAllGroupTodosAsync.request(groupSeq))
+  }
 
-    const onClickToGroupSettings = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        navigate(`/group/settings/${groupSeq}`)
-    }
+  const getUserList = () => {
+    dispatch(getUserListDataAsync.request(groupSeq))
+  }
 
-    const createTodo = (groupTodoInput: GroupTodoInput) => {
-        dispatch(saveGroupTodoAsync.request(groupTodoInput))
-    }
+  const onClickToGroupAnalysis = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    navigate(`/group/analysis/${groupSeq}`)
+  }
 
-    const startTodoTimer = (selectedGroupTodo: GroupTodo) => {
-        dispatch(changeUserTimerTodo(selectedGroupTodo))
-        navigate('/timer')
-    }
+  const onClickToGroupSettings = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    navigate(`/group/settings/${groupSeq}`)
+  }
 
-    const modifyTodo = (todoId: number, groupTodoInput: GroupTodoInput) => {
-        dispatch(modifyGroupTodoAsync.request({ todoId: todoId, groupTodoInput: groupTodoInput }))
-    }
+  const createTodo = (groupTodoInput: GroupTodoInput) => {
+    dispatch(saveGroupTodoAsync.request(groupTodoInput))
+  }
 
-    const deleteTodo = async (todoId: any) => {
-        await dispatch(deleteGroupTodoAsync.request(todoId))
-        await dispatch(findAllGroupTodosAsync.request(groupSeq))
-    }
+  const startTodoTimer = (selectedGroupTodo: GroupTodo) => {
+    dispatch(changeUserTimerTodo(selectedGroupTodo))
+    navigate('/timer')
+  }
 
-    const modifyTodoStatus = (todoId: number, todoStatus: string) => {
-        dispatch(
-            modifyTodoStatusAsync.request({
-                todoId: todoId,
-                todoStatus: { todoStatus: todoStatus },
-            })
-        )
-    }
-
-    return (
-        <>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'end',
-                    height: '9%',
-                }}
-            >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'end',
-                        height: '9%',
-                    }}
-                >
-                    <Button
-                        sx={{
-                            fontSize: '24px',
-                            color: (theme) => theme.colors.greenText,
-                            mx: '0.8vw',
-                            borderRadius: '0px',
-                        }}
-                        variant="text"
-                        color="success"
-                        onClick={onClickToGroup}
-                    >
-                        GROUP
-                    </Button>
-                    <Button
-                        sx={{
-                            fontSize: '24px',
-                            color: (theme) => theme.colors.greenText,
-                            mx: '0.8vw',
-                            borderBottom: (theme) => `2px solid ${theme.colors.greenText}`,
-                            borderRadius: '0px',
-                        }}
-                        variant="text"
-                        color="success"
-                    >
-                        BOARD
-                    </Button>
-                    <Button
-                        sx={{
-                            fontSize: '24px',
-                            color: (theme) => theme.colors.greenText,
-                            mx: '0.8vw',
-                            borderRadius: '0px',
-                            mb: '2px',
-                        }}
-                        variant="text"
-                        color="success"
-                        onClick={onClickToGroupAnalysis}
-                    >
-                        ANALYSIS
-                    </Button>
-                    <Button
-                        sx={{
-                            fontSize: '24px',
-                            color: (theme) => theme.colors.greenText,
-                            mx: '0.8vw',
-                            borderRadius: '0px',
-                            mb: '2px',
-                        }}
-                        variant="text"
-                        color="success"
-                        onClick={onClickToGroupSettings}
-                    >
-                        SETTING
-                    </Button>
-                </Box>
-                <OnTimer
-                    userList={userList}
-                    getUserList={getUserList}
-                />
-            </Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    height: '91%',
-                    mb: '3vh',
-                }}
-            >
-                <GroupTodoComponent
-                    group={group}
-                    groupSeq={groupSeq}
-                    groupTodos={groupTodos}
-                    createTodo={createTodo}
-                    modifyTodo={modifyTodo}
-                    deleteTodo={deleteTodo}
-                    modifyTodoStatus={modifyTodoStatus}
-                    startTodoTimer={startTodoTimer}
-                />
-            </Box>
-        </>
+  const modifyTodo = (todoId: number, groupTodoInput: GroupTodoInput) => {
+    dispatch(
+      modifyGroupTodoAsync.request({
+        todoId: todoId,
+        groupTodoInput: groupTodoInput,
+      })
     )
+  }
+
+  const deleteTodo = async (todoId: any) => {
+    await dispatch(deleteGroupTodoAsync.request(todoId))
+    await dispatch(findAllGroupTodosAsync.request(groupSeq))
+  }
+
+  const modifyTodoStatus = (todoId: number, todoStatus: string) => {
+    dispatch(
+      modifyTodoStatusAsync.request({
+        todoId: todoId,
+        todoStatus: { todoStatus: todoStatus },
+      })
+    )
+  }
+
+  return (
+    <>
+      <Grid
+        container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'end',
+          height: '10%',
+        }}
+      >
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6} sx={{ textAlign: 'center' }}>
+          <Button
+            sx={{
+              fontSize: '20px',
+              fontFamily: 'MapoPeacefull, TmoneyRoundWindRegular',
+              color: (theme) => theme.colors.greenText,
+              mx: '0.8vw',
+              borderBottom: (theme) => `2px solid ${theme.colors.greenText}`,
+              borderRadius: '0px',
+            }}
+            variant="text"
+            color="success"
+            onClick={(e: any) => e.preventDefault()}
+          >
+            BOARD
+          </Button>
+          <Button
+            sx={{
+              fontSize: '20px',
+              fontFamily: 'MapoPeacefull, TmoneyRoundWindRegular',
+              color: (theme) => theme.colors.greenText,
+              mx: '0.8vw',
+              borderRadius: '0px',
+              mb: '2px',
+            }}
+            variant="text"
+            color="success"
+            onClick={onClickToGroupAnalysis}
+          >
+            ANALYSIS
+          </Button>
+          <Button
+            sx={{
+              fontSize: '20px',
+              fontFamily: 'MapoPeacefull, TmoneyRoundWindRegular',
+              color: (theme) => theme.colors.greenText,
+              mx: '0.8vw',
+              borderRadius: '0px',
+              mb: '2px',
+            }}
+            variant="text"
+            color="success"
+            onClick={onClickToGroupSettings}
+          >
+            SETTING
+          </Button>
+        </Grid>
+        <Grid item xs={3} sx={{ pr: '5vw' }}>
+          <OnTimer userList={userList} getUserList={getUserList} />
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '90%',
+          mb: '3vh',
+        }}
+      >
+        <GroupTodoComponent
+          group={group}
+          groupSeq={groupSeq}
+          groupTodos={groupTodos}
+          createTodo={createTodo}
+          modifyTodo={modifyTodo}
+          deleteTodo={deleteTodo}
+          modifyTodoStatus={modifyTodoStatus}
+          startTodoTimer={startTodoTimer}
+        />
+      </Box>
+    </>
+  )
 }
 
 export default GroupTodoContainer
