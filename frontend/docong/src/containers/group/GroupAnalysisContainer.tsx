3f0898cabfc5f9a9f052af2f39250d4d22cc9bb2
유혜승 <hyeseung0124@gmail.com>
2022-04-05 23:45:27 +0900

    [S06P21S003-273] feat: Group Analysis 구현
    * 그룹 잔디 구현
    * 그룹 랭킹 구현
    * 그룹 시간대별 뽀모 구현

--- frontend/docong/src/containers/group/GroupAnalysisContainer.tsx
+++ frontend/docong/src/containers/group/GroupAnalysisContainer.tsx
@@ -1,10 +1,15 @@
-import { Box, Button } from '@mui/material'
-import { useEffect } from 'react'
+import { Box, Button, Divider } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
+import { UserData } from '../../api/user'
 import OnTimer from '../../components/group/OnTimer'
+import UserAllDateCountAnalysis from '../../components/user/UserAllDateCountAnalysis'
+import UserTimeCountAnalysis from '../../components/user/UserTimeCountAnalysis'
 import { RootState } from '../../modules'
 import { getGroupAllDateAnalysisAsync, getGroupRankingListAsync, getGroupTimeAnalysisAsync, getUserListDataAsync } from '../../modules/group'
+import { setUserInfoAsync } from '../../modules/user'
+import UserRanking from '../../components/user/UserRanking'
 
 function GroupAnalysisContainer() {
     const navigate = useNavigate()
@@ -12,21 +17,24 @@ function GroupAnalysisContainer() {
     const dispatch = useDispatch()
 
     const groupSeq = Number(params.groupSeq)
+    const userInfo = useSelector((state: RootState) => state.user.userInfo.data)
     const userList = useSelector((state: RootState) => state.group.userList.data)
-    
-    const groupAllDateAnalysis = useSelector((state: RootState) => 
-    state.group.groupAllDateAnalysis ? state.group.groupAllDateAnalysis.data : null)
 
-    const rankingList = useSelector((state: RootState) => 
-    state.group.rankingList ? state.group.rankingList.data : null)
+    const groupAllDateAnalysis = useSelector((state: RootState) =>
+        state.group.groupAllDateAnalysis ? state.group.groupAllDateAnalysis.data : null)
 
-    const groupTimeAnalysis = useSelector((state:RootState) =>
-    state.group.groupTimeAnalysis ? state.group.groupTimeAnalysis.data : null)
+    const rankingList = useSelector((state: RootState) =>
+        state.group.rankingList ? state.group.rankingList.data : null)
+
+    const groupTimeAnalysis = useSelector((state: RootState) =>
+        state.group.groupTimeAnalysis ? state.group.groupTimeAnalysis.data : null)
 
     const groupAllDateInput = {
         year: new Date().getFullYear(),
         groupSeq: groupSeq
     }
+
+    const [isOpenEditUserForm, setIsOpenEditUserForm] = useState(false)
 
     useEffect(() => {
         getUserList()
@@ -54,16 +62,22 @@ function GroupAnalysisContainer() {
         navigate(`/group/settings/${groupSeq}`)
     }
 
-    const check = () => {
-        console.log("groupAllDateAnalysis->",groupAllDateAnalysis)
-        console.log("rankingList->",rankingList)
-        console.log("groupTimeAnalysis->",groupTimeAnalysis)
+    const openEditUserForm = () => {
+        setIsOpenEditUserForm(true)
     }
 
+    const closeEditUserForm = () => {
+        setIsOpenEditUserForm(false)
+    }
 
-    return(
+    const editUser = (userData: UserData) => {
+        dispatch(setUserInfoAsync.request(userData))
+        setIsOpenEditUserForm(false)
+    }
+
+    return (
         <>
-        <Box
+            <Box
                 sx={{
                     display: 'flex',
                     justifyContent: 'center',
@@ -131,7 +145,136 @@ function GroupAnalysisContainer() {
                     getUserList={getUserList}
                 />
             </Box>
-            <Button onClick={check}>console</Button>
+            <Box
+                sx={{
+                    display: 'flex',
+                    height: '90%',
+                    px: '3vw',
+                    pt: '3vh',
+                    pb: '5vh',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        width: '100%',
+                        justifyContent: 'center',
+                        flexGrow: 1,
+                        overflow: 'overlay',
+                        '> div': { width: '100%' },
+                        '::-webkit-scrollbar': {
+                            width: '0px',
+                        },
+                        '::-webkit-scrollbar-thumb': {
+                            backgroundColor: '#8a9788',
+                            borderRadius: '8px',
+                            backgroundClip: 'padding-box',
+                            border: '1px solid transparent',
+                        },
+                        '::-webkit-scrollbar-track': {
+                            backgroundColor: '#c4c4c4',
+                            borderRadius: '8px',
+                            boxShadow: 'inset 0px 0px 5px white',
+                        },
+                        '&:hover': {
+                            '::-webkit-scrollbar': {
+                                width: '8px',
+                            },
+                        },
+                    }}
+                >
+                    {userInfo && !userInfo.birth && (
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    fontSize: '20px ',
+                                    fontWeight: 'bold',
+                                    cursor: 'pointer',
+                                    color: (theme) => theme.colors.greenText,
+                                    textDecoration: 'underline',
+                                }}
+                                onClick={openEditUserForm}
+                            >
+                                추가 정보
+                            </Box>
+                            <Box sx={{ fontSize: '20px' }}>
+                                를 입력하시면 사용 통계를 확인할 수 있습니다.
+                            </Box>
+                        </Box>
+                    )}
+                    {userInfo && userInfo.birth && (
+                        <Box sx={{ px: '2vw', py: '1vh' }}>
+                            <Box
+                                sx={{
+                                    boxShadow: 2,
+                                    width: '100%',
+                                    height: '17vw',
+                                    p: '20px',
+                                    mb: '1.5vw',
+                                    borderRadius: '8px',
+                                }}
+                            >
+                                <Box sx={{ textAlign: 'center', fontSize: '1.8vw' }}>
+                                    두콩밭
+                                </Box>
+                                <Divider sx={{ my: '0.3vw' }} variant="middle" />
+                                <UserAllDateCountAnalysis
+                                    userAllDateAnalysis={groupAllDateAnalysis}
+                                />
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    mb: '1.5vw',
+                                }}
+                            >
+                                <Box sx={{
+                                    boxShadow: 2,
+                                    width: '49%',
+                                    height: '21vw',
+                                    p: '20px',
+                                    borderRadius: '8px',
+                                }}>
+                                    <Box
+                                        sx={{
+                                            textAlign: 'center',
+                                            fontSize: '36px',
+                                            mt: '1vh',
+                                            color: (theme) => theme.colors.greenText,
+                                        }}
+                                    >
+                                        두콩 랭킹
+                                    </Box>
+                                    <UserRanking rankingList={rankingList} />
+                                </Box>
+                                <Box
+                                    sx={{
+                                        boxShadow: 2,
+                                        width: '49%',
+                                        height: '21vw',
+                                        p: '20px',
+                                        borderRadius: '8px',
+                                    }}
+                                >
+                                    <Box
+                                        sx={{ textAlign: 'center', fontSize: '1.6vw', mb: '1vw' }}
+                                    >
+                                        시간대별 통계
+                                    </Box>
+                                    <UserTimeCountAnalysis userTimeAnalysis={groupTimeAnalysis} />
+                                </Box>
+                            </Box>
+                        </Box>
+                    )}
+                </Box>
+            </Box>
         </>
     )
 }
